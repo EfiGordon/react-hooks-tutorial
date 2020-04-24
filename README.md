66bf9a2b875c6fdc5e9bd442eb67f8f1ab03e4ec
Efi Gordon <efigordon90@gmail.com>
2020-04-24 18:11:47 +0300

    add useEffect and update readme

--- README.md
+++ README.md
@@ -20,3 +20,46 @@ const IngredientForm = React.memo(props => {
 ...
 }
 ```
+
+
+# React hooks
+## useState()
+This is the most popular hook
+,the common use case is like this:
+```
+    const [currentState, setCurrentStateFunction] = useState('some inital state, can be any type such string, array, object, etc..')
+```
+
+for updating the state:
+
+```
+setCurrentStateFunction('the new state')
+```
+
+## useEffect()
+As its name can be implied, we use this hook when we want to make a side effects such as http request.
+
+The following behavior is like componentDid**Update**
+```
+useEffect(() => {
+    fetch(URL)
+    .then(
+    response => {
+        // some logic
+    }
+    )
+})
+```
+
+The following behavior is like componentDid**Mount**
+(if there are variables in the array, the useEffect function will be executed only if its effect them.)
+```
+useEffect(() => {
+    fetch(URL)
+    .then(
+    response => {
+        // some logic
+    }
+    )
+},[])
+```
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,23 +6,60 @@ import IngredientList from './IngredientList';
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  const addIngredientHandler = ingredient => {
-    setUserIngredients(
-      (prevIngredients) => {
-        return [...prevIngredients, {
-          id: Math.random().toString(),
-          ...ingredient
-        }];
-      }
-    )
+  const removeIngredientHandler = ingredientId => {
+    const newUserIngredients = userIngredients.filter(ing => ing.id !== ingredientId);
+    setUserIngredients(newUserIngredients);
   }
+
+  useEffect(() => {
+    fetch('https://react-hooks-97ab7.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = []
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      })
+
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-97ab7.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((responseData) => {
+      setUserIngredients(
+        (prevIngredients) => {
+          return [...prevIngredients, {
+            id: responseData.name,
+            ...ingredient
+          }];
+        }
+      )
+    });
+
+
+  }
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => { }} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={(ingredientId) => {
+          removeIngredientHandler(ingredientId);
+        }} />
       </section>
     </div>
   );
